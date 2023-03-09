f45f15979b5ab992b48c549de57fedc0ae0fd850
Alex <alexiancu50@protonmail.com>
2023-03-09 11:21:29 +0200

    Created next.js app

---
+++ nextjs-frontend/pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Auction</title>
+        <meta name="description" content="Auction app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1 stlye="center">Hello World!</h1>
+    </div>
+  );
+}
